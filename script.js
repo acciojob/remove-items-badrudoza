db19527c24fa1f2731bc66a974885eee31eafed6
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-09 07:11:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let input=document.querySelector("input");
+input.addEventListener("click",function(e){
+	e.preventDefault()
+	let select=document.querySelector("#colorSelect");
+	select.remove(select.selectedIndex);
+});
